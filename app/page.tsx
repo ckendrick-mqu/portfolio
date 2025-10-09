52143a63f8f4e2762d3bc3b8740cee7b76bacc6c
ckendrick-mqu <christopher.kendrick1@students.mq.edu.au>
2025-10-09 19:12:46 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Christopher Kendrick
+      </h1>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Portfolio
       </h1>
       <p className="mb-4">
         {`I made game.`}
