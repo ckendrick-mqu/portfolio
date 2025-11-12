fe20bd893647936f91798ad25ec732d33727e107
ckendrick-mqu <christopher.kendrick1@students.mq.edu.au>
2025-11-12 11:35:20 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Page() {
       <p className="mb-4">
         {`IT student focused on software and games development, experienced in Agile projects and Unity/Java development.`}
       </p>
-        
-        <a href="https://ckendrick-mqu.itch.io/counter-knights-arcane-offensive">Counter Knights: Arcane Offensive (Click to play in your browser)</a><br></br>
-        <a href="https://github.com/ckendrick-mqu/COMP1010">Dungeons & Dungeons & Dungeons (Source code only)</a><br></br>
+      <p className="mb-4">
+        {`My projects can be found in the below blog posts.`}
+      </p>
       <div className="my-8">
         <BlogPosts />
       </div>
