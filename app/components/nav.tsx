d9c458daf8eb39bfa3da285c4209029d1102cc24
ckendrick-mqu <christopher.kendrick1@students.mq.edu.au>
2025-11-12 12:02:31 +1100

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -7,6 +7,9 @@ const navItems = {
   '/blog': {
     name: 'blog',
   },
+  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
+    name: 'deploy',
+  },
 }
 
 export function Navbar() {
